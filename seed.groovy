321f6d0f5d1304a7a363917379f1bd38196856ca
Kumar <saurabh.e.kumar@capgemini.com>
2017-01-20 16:44:12 +0530

    SeedJOb changed added Pipeline View

--- seed.groovy
+++ seed.groovy
@@ -1,25 +1,35 @@
+buildPipelineView('OCD') {
+    filterBuildQueue()
+    filterExecutors()
+    title('One Click Deployment CI Pipeline')
+    displayedBuilds(1)
+    selectedJob('OneClickDeployment/compile_Job1')
+    alwaysAllowManualTrigger()
+    showPipelineParameters()
+    refreshFrequency(60)
+
 mavenJob('OneClickDeployment/compile_Job1') {
-    scm {
+ scm {
 	    github('saurau01/OneClickDemo', 'master')
 	        }
-			triggers {
-			        snapshotDependencies(true)
-						authenticationToken('capgemini')
-						    }
-						    	rootPOM('pom.xml')
+triggers {
+          snapshotDependencies(true)
+          authenticationToken('capgemini')
+         }
+rootPOM('pom.xml')
 							    
-							        goals('compile')
-								}
+goals('compile')
+}
 
 
-								mavenJob('OneClickDeployment/Test_Job2') {
-								    scm {
-								            github('assahoo/mvn-test', 'master')
-									        }
-											triggers {
-											        upstream('OneClickDeployment/compile_Job1', 'SUCCESS')
-												    }
-												    	rootPOM('pom.xml')
+mavenJob('OneClickDeployment/Test_Job2') {
+ scm {
+     github('assahoo/mvn-test', 'master')
+     }
+ triggers {
+           upstream('OneClickDeployment/compile_Job1', 'SUCCESS')
+          }
+ rootPOM('pom.xml')
 													    
-													        goals('test')
-														}
+        goals('test')
+}
