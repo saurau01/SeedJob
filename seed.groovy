a18928fbab7dbcb4993276b4b5860299488712e1
Kumar <saurabh.e.kumar@capgemini.com>
2017-01-20 16:22:37 +0530

    SeedJOb

---
+++ seed.groovy
@@ -0,0 +1,25 @@
+mavenJob('OneClickDeployment/compile_Job1') {
+    scm {
+            github('saurau01/OneClickDemo.git', 'master')
+	        }
+			triggers {
+			        snapshotDependencies(true)
+						authenticationToken('capgemini')
+						    }
+						    	rootPOM('pom.xml')
+							    
+							        goals('compile')
+								}
+
+
+								mavenJob('OneClickDeployment/Test_Job2') {
+								    scm {
+								            github('assahoo/mvn-test', 'master')
+									        }
+											triggers {
+											        upstream('OneClickDeployment/compile_Job1', 'SUCCESS')
+												    }
+												    	rootPOM('pom.xml')
+													    
+													        goals('test')
+														}
