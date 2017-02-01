2107fb5243ac641ca914f7761a1607bb9b3d2df4
SAURABH KUMAR <saurabhkumar@SAURABHs-MacBook-Pro.local>
2017-02-01 08:54:24 +0530

    Modified Build Script to copy artifact on S3

--- Build_Job3.groovy
+++ Build_Job3.groovy
@@ -13,6 +13,13 @@ mavenJob('OneClickDeployment/Build_Job3') {
         archiveArtifacts('target/*.war')
 		
         downstream('OneClickDeployment/Deploy_Job4', 'SUCCESS')
+        s3('s3upload') {
+            entry('target/*.war', 'vagrant-jenkins', 'us-east-1') {
+                storageClass('REDUCED_REDUNDANCY')
+                noUploadOnFailure()
+                uploadFromSlave()
+            }
+        }
     
     }
 }
