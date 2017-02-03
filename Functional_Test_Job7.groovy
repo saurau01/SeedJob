d568dff352ac78f2665cea8c17046a53abddf8ed
SAURABH KUMAR <saurabhkumar@SAURABHs-MacBook-Pro.local>
2017-02-03 10:02:43 +0530

    Added functional test Job

--- Functional_Test_Job7.groovy
+++ Functional_Test_Job7.groovy
@@ -3,7 +3,7 @@ freeStyleJob('OneClickDeployment/Functional_Test_Job7') {
 steps {
 shell(''' 
 #!/bin/sh
-home=/Users/Shared/Jenkins/Home
+home=/private/var/root
 phantomjs=/Users/Shared/Jenkins/Home/workspace/OneClickDeployment/DeployCode_Job6/phantomjs-2.1.1-macosx/bin
 export PATH=$PATH:$phantomjs
 python $phantomjs/../../test.py
